84c1e0df2d248f37d7d018139e39356ab5a50bd7
Yuldashev222 <oybekyuldashov54@gmail.com>
2022-03-12 10:56:42 +0500

    director add postda user bn employee aralashib qoldi

--- main/views.py
+++ main/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import redirect, render
 
 from .models import Company
+from post.models import Post
+from product.models import Product
+
+
+from .forms import CompanyForm
+from post.forms import PostForm
+from product.forms import ProductForm
 
 def home(request):
     return render(request, 'index.html')
@@ -52,4 +59,51 @@ def delete_company(request, url):
 def edit_company(request, url):
     company = Company.objects.get(url=url)
 
-    return render(request, 'edit_company.html')
+    company_form = CompanyForm(instance=company)
+
+    add_post_form = PostForm()
+    add_product_form = ProductForm()
+
+    posts = Post.objects.filter(company=company)
+    products = Product.objects.filter(company=company)
+
+    
+
+    if request.POST:
+
+        company_form = CompanyForm(request.POST or None, request.FILES or None, instance=company)
+
+        add_post_form = PostForm(request.POST or None, request.FILES or None)
+        add_product_form = ProductForm(request.POST or None, request.FILES or None)
+
+        if company_form.is_valid():
+
+            obj = company_form.save(commit=False)
+            obj.author = company.author
+            obj.save()
+        
+        if add_post_form.is_valid():
+            obj = add_post_form.save(commit=False)
+            obj.company = company
+            obj.author = company.author
+            obj.save()
+
+        if add_product_form.is_valid():
+            obj = add_product_form.save(commit=False)
+            obj.company = company
+            obj.author = company.author
+            obj.save()
+
+            return redirect('edit_company', url)
+
+    context = {
+        'company': company,
+        'company_form': company_form,
+        'add_post_form': add_post_form,
+        'add_product_form': add_product_form,
+
+        'posts': posts,
+        'products': products,
+    }
+
+    return render(request, 'edit_company.html', context)
